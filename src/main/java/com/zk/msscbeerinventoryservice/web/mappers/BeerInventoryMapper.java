a2b30c759731cda7c312b10fa5b8aceb1b47aff8
ZaeemHasan <syedzaeem25@gmail.com>
2020-11-14 18:29:54 +0100

    Initial commit

---
+++ src/main/java/com/zk/msscbeerinventoryservice/web/mappers/BeerInventoryMapper.java
@@ -0,0 +1,13 @@
+package com.zk.msscbeerinventoryservice.web.mappers;
+
+import com.zk.msscbeerinventoryservice.domain.BeerInventory;
+import com.zk.msscbeerinventoryservice.web.model.BeerInventoryDto;
+import org.mapstruct.Mapper;
+
+@Mapper(uses = {DateMapper.class})
+public interface BeerInventoryMapper {
+
+    BeerInventory beerInventoryDtoToBeerInventory(BeerInventoryDto beerInventoryDTO);
+
+    BeerInventoryDto beerInventoryToBeerInventoryDto(BeerInventory beerInventory);
+}
